4abba53782447ececc5d47791650a2568f514315
somecodingguyatthecoderschool <1051110@etusd.org>
2024-11-12 17:57:38 -0800

    i added more code

--- main.py
+++ main.py
@@ -3,3 +3,38 @@ import pygame
 import random
 
 pygame.init()
+
+# class Dog:
+
+#     def __init__(self):
+#         self.age=7
+#         self.name="ralph"
+#     def jump(self):
+#         print ("jumpingupanddown")
+
+# dog = Dog()
+# dog.jump()
+BLOCKSIZE = 30
+GRID_WIDTH = 10
+GRID_HEIGHT = 20
+SCREEN_WIDTH = 1010
+SCREEN_HEIGHT = 800
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+COLORS = [
+    (235, 171, 52), #yellow
+    (255,0,0), #red
+    (0,255,0), #green
+    (0,0,255), #blue
+    (235, 131, 52), #orange
+    (52, 217, 235), #cyan
+    (128, 52, 235)  #purple
+]
+SHAPES = [
+    [[1,1], [1,1]], #BLOCKSHAPE
+    [[1,1,0], [0,1,1]], #Z SHAPE
+    
+          
+          ]
---
+++ practice.py
@@ -0,0 +1,15 @@
+anime_shows = [ "bleach","naruto","one piece"]
+
+video_games = ["mw3","gta V","minecraft"]
+
+
+food =        [ "pizza", "chicken","raviolli"]
+
+
+
+multi_list = [
+    anime_shows,
+    video_games,
+    food
+]
+print (multi_list[0][1])
